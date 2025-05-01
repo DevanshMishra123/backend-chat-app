be8477826ceffb3af9d256fa28d3b1f517655d64
DevanshMishra123 <mishra24devansh@gmail.com>
2025-05-02 00:37:44 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+import { createServer } from "http";
+import express from "express";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "https://chat-box-app-imqn.vercel.app", 
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("send_message", (data) => {
+    socket.broadcast.emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Socket.IO server running on port 3000");
+});
